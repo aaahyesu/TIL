f4e482d1c287396bca0f02402d1ce5a6defc0673
Hyesu <skxldzm123@naver.com>
2024-09-09 15:16:48 +0900

    add: TodoList-CRUD

---
+++ [React]ToDoList/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import "./Editor.css";
+import { useState, useRef } from "react";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  // 엔터 시 입력
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    // 아무것도 입력하지 않았을 때 focus & 입력 X
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent(""); // 입력 후 content 초기화
+  };
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        type="text"
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
