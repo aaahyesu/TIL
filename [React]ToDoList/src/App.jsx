121644f4984b87cc2cc05602ca3795b6d3c1fbf2
Hyesu <skxldzm123@naver.com>
2024-09-09 15:43:04 +0900

    add: useReducer 학습

--- [React]ToDoList/src/App.jsx
+++ [React]ToDoList/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
+import Exam from "./components/Exam";
 
 const mockData = [
   {
@@ -55,9 +56,10 @@ function App() {
   };
   return (
     <div className="App">
-      <Header />
+      <Exam />
+      {/* <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} /> */}
     </div>
   );
 }
